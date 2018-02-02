d9ec89ec5fe2eb29bd3e6a1ed32f0b14290b0b4c
mayuan <mayuanucas@qq.com>
2018-02-02 19:42:28 +0800

    解题

---
+++ ac/02/Solution03.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+/**
+ * @author mayuan
+ * @desc
+ * @date 2018/02/02
+ */
+public class Solution03 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    }
+
+}
