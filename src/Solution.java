fa86c2cea4a4d8a6f9234f0c3723b8a938f1c86a
mayuan <mayuanucas@qq.com>
2018-02-04 16:35:10 +0800

    解题模板

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,3 @@
-import java.io.*;
-
 /**
  * @author mayuan
  * @desc
@@ -7,11 +5,44 @@ import java.io.*;
  */
 public class Solution {
 
-    public static void main(String[] args) throws IOException{
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args){
+        test();
+    }
 
+    public static void test() {
+        int[][] numbers = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+        int target = 7;
+        if (!findNumber(numbers, target)) {
+            System.out.println("None");
+        } else {
+            System.out.println("\n------------------------");
+        }
 
+    }
+
+    public static boolean findNumber(int[][] numbers, int target) {
+        if (null == numbers || 0 == numbers.length) {
+            return false;
+        }
+
+        int rows = numbers.length;
+        int column = numbers[0].length;
+        int r = 0;
+        int c = column - 1;
+        boolean flag = false;
+
+        while (r < rows && 0 <= c) {
+            if (numbers[r][c] == target) {
+                System.out.print(target);
+                flag = true;
+                break;
+            } else if (numbers[r][c] > target) {
+                --c;
+            } else {
+                ++r;
+            }
+        }
+        return flag;
     }
 
 }
