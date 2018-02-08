5b1116fcd91aa90c83a29ea70293a4145f6f16b9
mayuan <mayuanucas@qq.com>
2018-02-08 17:14:43 +0800

    解题

---
+++ ac/LeetCode/Solution020.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+/**
+ * @author mayuan
+ * @desc
+ * @date 2018/02/08
+ */
+public class Solution020 {
+
+    public boolean isValid(String s) {
+        if (null == s || (1 == (s.length() & 1))){
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        for (int i=0; i<s.length(); i++){
+            switch (s.charAt(i)){
+                case '(':
+                    stack.push(')');
+                    break;
+                case '[':
+                    stack.push(']');
+                    break;
+                case '{':
+                    stack.push('}');
+                    break;
+                case ')':
+                case ']':
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != s.charAt(i)){
+                        return false;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+}
