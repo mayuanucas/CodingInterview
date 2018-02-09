faca44e20ccc3ee582f0f600544c3c3d0a4eca9f
mayuan <mayuanucas@qq.com>
2018-02-09 11:23:43 +0800

    美好的一天从解题开始

---
+++ ac/LeetCode/Solution028.java
@@ -0,0 +1,24 @@
+/**
+ * @author mayuan
+ * @desc
+ * @date 2018/02/09
+ */
+public class Solution028 {
+
+    public int strStr(String haystack, String needle) {
+        for (int i = 0; ; i++) {
+            for (int j = 0; ; j++) {
+                if (j == needle.length()) {
+                    return i;
+                }
+                if (i + j == haystack.length()) {
+                    return -1;
+                }
+                if (needle.charAt(j) != haystack.charAt(i + j)) {
+                    break;
+                }
+            }
+        }
+    }
+
+}
