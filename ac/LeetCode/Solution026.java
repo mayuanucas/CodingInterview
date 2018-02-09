15a955774cc0cc8a3cb28a5f8f64dd93c53a9e6c
mayuan <mayuanucas@qq.com>
2018-02-09 09:05:53 +0800

    美好的一天从解题开始

---
+++ ac/LeetCode/Solution026.java
@@ -0,0 +1,20 @@
+/**
+ * @author mayuan
+ * @desc
+ * @date 2018/02/09
+ */
+public class Solution026 {
+
+    public int removeDuplicates(int[] nums) {
+        int i = nums.length > 0 ? 1 : 0;
+
+        for (int number : nums) {
+            if (number > nums[i - 1]) {
+                nums[i++] = number;
+            }
+        }
+
+        return i;
+    }
+
+}
