7937a19a583a444d25ae21ed4fc021d196523cf5
mayuan <mayuanucas@qq.com>
2018-02-10 21:13:50 +0800

    解题模板

--- src/Solution.java
+++ src/Solution.java
@@ -1,40 +1,12 @@
 /**
  * @author mayuan
  * @desc
- * @date 2018/02/07
+ * @date 2018/02/10
  */
 public class Solution {
 
     public static void main(String[] args){
-        test();
-    }
-
-    /**
-     *
-     */
-    public static void test() {
-        System.out.println("fib(3)= " + fibonacci(3));
-        System.out.println("fib(10)= " + fibonacci(10));
-    }
-
-    public static long fibonacci(int n){
-        int[] result = {0, 1};
-        if (n < 2){
-            return result[n];
-        }
-
-        long a = 0;
-        long b = 1;
-        long fibNumber = 0;
-        for (int i=2; i<=n; ++i){
-            fibNumber = a + b;
-
-            a = b;
-            b = fibNumber;
-        }
-
-        return fibNumber;
-
+        System.out.println("Hello Java!");
     }
 
 }
