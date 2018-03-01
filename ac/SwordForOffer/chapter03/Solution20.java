0e15b43d9628a4cde126110e3db02c055562b0b7
mayuan <mayuanucas@qq.com>
2018-03-01 19:36:51 +0800

    正则表达式判断字符串是否为数值表示

---
+++ ac/SwordForOffer/chapter03/Solution20.java
@@ -0,0 +1,22 @@
+/**
+ * @author mayuan
+ * @desc
+ * @date 2018/03/01
+ */
+public class Solution20 {
+
+    public static void main(String[] args){
+        System.out.println(isNumeric("12e"));
+        System.out.println(isNumeric("1e3.14"));
+        System.out.println(isNumeric("1e-3.14"));
+        System.out.println(isNumeric("1e-3"));
+        System.out.println(isNumeric("1e3"));
+    }
+
+    public static boolean isNumeric(String text){
+        if (null == text || 0 >= text.length()){
+            return false;
+        }
+        return text.matches("[\\+-]?[0-9]*(\\.[0-9]*)?([eE][\\+-]?[0-9]+)?");
+    }
+}
