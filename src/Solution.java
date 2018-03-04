58c71893261bc085845f88a7392534ff2b71a377
mayuan <mayuanucas@qq.com>
2018-03-04 10:06:30 +0800

    修改答题模板

--- src/Solution.java
+++ src/Solution.java
@@ -1,12 +1,12 @@
 /**
  * @author mayuan
  * @desc
- * @date 2018/03/03
+ * @date 2018/03/04
  */
 public class Solution {
 
     public static void main(String[] args){
-        System.out.println("Hello Java!");
+
     }
 
 }
