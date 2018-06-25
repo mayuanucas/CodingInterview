1ae5419547b60a7de2489fb80dca0048f4a93510
mayuan <mayuanucas@qq.com>
2018-06-25 10:31:27 +0800

    最长回文子串-2种解法

--- algorithm/LeetCode/Solution005.java
+++ algorithm/LeetCode/Solution005.java
@@ -5,6 +5,36 @@
  */
 public class Solution005 {
 
+    public static void main(String[] args){
+        Solution005 sol = new Solution005();
+
+        System.out.println(sol.longestPalindrome2("abba"));
+    }
+
+    public String longestPalindrome2(String s) {
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int l1 = expansion(s, i, i);
+            int l2 = expansion(s, i, i + 1);
+            int len = Math.max(l1, l2);
+            if (len > end - start) {
+                // 当回文子串长度为偶数时，需先减一
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public int expansion(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+
     public String longestPalindrome(String s) {
         int maxLength = 0, start = 0;
         // 第一种情况： aba 以 i 为中心向两边扩展
