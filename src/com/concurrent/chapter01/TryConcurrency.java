9506353dd37e6815e8107b4d70a00818c2219425
mayuan <mayuanucas@qq.com>
2018-06-27 19:32:30 +0800

    Java并发基础

---
+++ src/com/concurrent/chapter01/TryConcurrency.java
@@ -0,0 +1,42 @@
+package com.concurrent.chapter01;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/06/27
+ */
+public class TryConcurrency {
+    public static void main(String[] args) {
+        new Thread(TryConcurrency::enjoyMusic).start();
+        new Thread(TryConcurrency::browseNews).start();
+    }
+
+    private static void browseNews() {
+        while (true) {
+            System.out.println("游览网页中...");
+            sleep(1);
+        }
+    }
+
+    private static void enjoyMusic() {
+        while (true) {
+            System.out.println("听音乐中...");
+            sleep(1);
+        }
+    }
+
+    private static void sleep(int seconds) {
+        if (seconds < 0) {
+            seconds = 0;
+        }
+
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
