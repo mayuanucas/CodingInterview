7f8966f4d14bcccc33a98f51a395c14eb817024d
mayuan <mayuanucas@qq.com>
2018-06-29 20:15:57 +0800

    yield 的使用

---
+++ src/com/concurrent/cpt03/ThreadYield.java
@@ -0,0 +1,25 @@
+package com.concurrent.cpt03;
+
+import java.util.stream.IntStream;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/06/29
+ */
+public class ThreadYield {
+
+    public static void main(String[] args) {
+        IntStream.range(0, 2).mapToObj(ThreadYield::create).forEach(Thread::start);
+    }
+
+    private static Thread create(int index) {
+        return new Thread(() -> {
+            if (0 == index) {
+                Thread.yield();
+            }
+            System.out.println(index);
+        });
+    }
+
+}
