e3ec94af0a653e75045928627c398094d34e6287
mayuan <mayuanucas@qq.com>
2018-06-30 22:45:41 +0800

    线程的中断

---
+++ src/com/concurrent/cpt03/ThreadInterrupted3.java
@@ -0,0 +1,30 @@
+package com.concurrent.cpt03;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/06/30
+ */
+public class ThreadInterrupted3 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    boolean fl = Thread.interrupted();
+                    if (fl){
+                        System.out.print("**************");
+                    }
+                    System.out.println(fl);
+                }
+            }
+        };
+        thread.setDaemon(true);
+        thread.start();
+
+        TimeUnit.MILLISECONDS.sleep(2);
+        thread.interrupt();
+    }
+}
---
+++ src/com/concurrent/cpt03/ThreadInterrupted4.java
@@ -0,0 +1,24 @@
+package com.concurrent.cpt03;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/06/30
+ */
+public class ThreadInterrupted4 {
+    public static void main(String[] args) {
+        System.out.println("Main thread is interrupted? " + Thread.interrupted());
+
+        Thread.currentThread().interrupt();
+
+        System.out.println("Main thread is interrupted? " + Thread.currentThread().isInterrupted());
+
+        try {
+            TimeUnit.MINUTES.sleep(1);
+        } catch (InterruptedException e) {
+            System.out.println("I will be interrupted still.");
+        }
+    }
+}
