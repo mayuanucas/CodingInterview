68041cb4c92943369be027066086d61cde5477aa
mayuan <mayuanucas@qq.com>
2018-06-30 21:46:52 +0800

    获取当前线程名

---
+++ src/com/concurrent/cpt03/CurrentThread.java
@@ -0,0 +1,23 @@
+package com.concurrent.cpt03;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/06/30
+ */
+public class CurrentThread {
+    public static void main(String[] args) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                System.out.println("当前线程名:" + Thread.currentThread().getName());
+                System.out.println(Thread.currentThread() == this);
+            }
+        };
+        thread.start();
+
+        String name = Thread.currentThread().getName();
+        System.out.println("当前线程名:" + name);
+        System.out.println("main".equals(name));
+    }
+}
