cb3835a7da6a522e56e5f6a5c6338c4e23a63aa2
mayuan <mayuanucas@qq.com>
2018-07-01 10:28:24 +0800

    线程join 方法

---
+++ src/com/concurrent/cpt03/ThreadJoin.java
@@ -0,0 +1,46 @@
+package com.concurrent.cpt03;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/07/01
+ */
+public class ThreadJoin {
+    public static void main(String[] args) throws InterruptedException {
+        List<Thread> threads = IntStream.range(1, 3).mapToObj(ThreadJoin::create).collect(toList());
+        threads.forEach(Thread::start);
+
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + "#" + i);
+            shortSleep();
+        }
+    }
+
+    private static Thread create(int seq) {
+        return new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(Thread.currentThread().getName() + "#" + i);
+                shortSleep();
+            }
+        }, String.valueOf(seq));
+    }
+
+    private static void shortSleep() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
