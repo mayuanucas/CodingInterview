89b898159fb8b3af382529bd75314b91feab9517
mayuan <mayuanucas@qq.com>
2018-07-03 20:41:28 +0800

    线程间通信

---
+++ src/com/concurrent/cpt05/EventClient.java
@@ -0,0 +1,31 @@
+package com.concurrent.cpt05;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/07/03
+ */
+public class EventClient {
+
+    public static void main(String[] args) {
+        final EventQueue eventQueue = new EventQueue();
+        new Thread(() -> {
+            while (true) {
+                eventQueue.offer(new EventQueue.Event());
+            }
+        }, "Producer").start();
+
+        new Thread(() -> {
+            while (true) {
+                eventQueue.take();
+                try {
+                    TimeUnit.SECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "Consumer").start();
+    }
+}
---
+++ src/com/concurrent/cpt05/EventQueue.java
@@ -0,0 +1,67 @@
+package com.concurrent.cpt05;
+
+import java.util.LinkedList;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/07/03
+ */
+public class EventQueue {
+
+    static class Event {
+    }
+
+    private int max;
+
+    private final LinkedList<Event> eventQueue = new LinkedList<>();
+
+    private final static int DEFAULT_MAX_EVENT = 10;
+
+    public EventQueue() {
+        this(DEFAULT_MAX_EVENT);
+    }
+
+    public EventQueue(int max) {
+        this.max = max;
+    }
+
+    public void offer(Event event) {
+        synchronized (eventQueue) {
+            if (eventQueue.size() >= max) {
+                try {
+                    print(" the queue is full.");
+                    eventQueue.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            print(" the new event is submitted.");
+            eventQueue.addLast(event);
+            eventQueue.notify();
+        }
+    }
+
+    public Event take() {
+        synchronized (eventQueue) {
+            if (eventQueue.isEmpty()) {
+                try {
+                    print(" the queue is empty.");
+                    eventQueue.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            Event event = eventQueue.removeFirst();
+            this.eventQueue.notify();
+            print(" the event " + event + " is handled.");
+            return event;
+        }
+    }
+
+    public void print(String message) {
+        System.out.printf("%s:%s\n", Thread.currentThread().getName(), message);
+    }
+
+}
