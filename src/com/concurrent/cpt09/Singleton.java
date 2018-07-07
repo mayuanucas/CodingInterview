e711cca7b57241a2f759671395ebd97f055dce36
mayuan <mayuanucas@qq.com>
2018-07-07 19:58:44 +0800

    类的初始化

---
+++ src/com/concurrent/cpt09/Singleton.java
@@ -0,0 +1,30 @@
+package com.concurrent.cpt09;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/07/07
+ */
+public class Singleton {
+
+    private static int x = 0;
+
+    private static int y;
+
+    private static Singleton instance = new Singleton();
+
+    private Singleton() {
+        x++;
+        y++;
+    }
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        System.out.println(singleton.x);
+        System.out.println(singleton.y);
+    }
+}
---
+++ src/com/concurrent/cpt09/Singleton2.java
@@ -0,0 +1,30 @@
+package com.concurrent.cpt09;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/07/07
+ */
+public class Singleton2 {
+
+    private static Singleton2 instance = new Singleton2();
+
+    private static int x = 0;
+
+    private static int y;
+
+    private Singleton2() {
+        x++;
+        y++;
+    }
+
+    public static Singleton2 getInstance() {
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        Singleton2 singleton = Singleton2.getInstance();
+        System.out.println(singleton.x);
+        System.out.println(singleton.y);
+    }
+}
