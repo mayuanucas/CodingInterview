96a9314728f4bd47a1fc7e077a9225785e220169
mayuan <mayuanucas@qq.com>
2018-07-07 14:38:28 +0800

    初始化子类会导致父类初始化

---
+++ src/com/concurrent/cpt09/ActiveLoadTest.java
@@ -0,0 +1,12 @@
+package com.concurrent.cpt09;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/07/07
+ */
+public class ActiveLoadTest {
+    public static void main(String[] args) {
+        System.out.println(Child.x);
+    }
+}
