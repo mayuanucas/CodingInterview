b2c7953eefe07e5b333631226f91856bad33d98a
mayuan <mayuanucas@qq.com>
2018-07-09 15:03:40 +0800

    JVM 内置三大类加载器

---
+++ src/com/concurrent/cpt10/ApplicationClassLoader.java
@@ -0,0 +1,13 @@
+package com.concurrent.cpt10;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/07/09
+ */
+public class ApplicationClassLoader {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("java.class.path"));
+        System.out.println(ApplicationClassLoader.class.getClassLoader());
+    }
+}
---
+++ src/com/concurrent/cpt10/BootstrapClassLoader.java
@@ -0,0 +1,13 @@
+package com.concurrent.cpt10;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/07/09
+ */
+public class BootstrapClassLoader {
+    public static void main(String[] args) {
+        System.out.println("Bootstrap:" + String.class.getClassLoader());
+        System.out.println(System.getProperty("sun.boot.class.path"));
+    }
+}
