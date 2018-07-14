069059c4be10d8bc468bba8ff5c67fbf923a4d4f
mayuan <mayuanucas@qq.com>
2018-07-14 13:22:37 +0800

    原型模式

---
+++ src/design/pattern/prototype/ConcretePrototype.java
@@ -0,0 +1,25 @@
+package design.pattern.prototype;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/07/14
+ */
+public class ConcretePrototype extends Prototype {
+
+    private String field;
+
+    public ConcretePrototype(String f) {
+        this.field = f;
+    }
+
+    @Override
+    Prototype myClone() {
+        return new ConcretePrototype(field);
+    }
+
+    @Override
+    public String toString() {
+        return field;
+    }
+}
