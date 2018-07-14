e6e8bd19423782a1d1758f08c73028a867c8f822
mayuan <mayuanucas@qq.com>
2018-07-14 10:05:15 +0800

    简单工厂

---
+++ src/design/pattern/simplefactory/Product.java
@@ -0,0 +1,9 @@
+package design.pattern.simplefactory;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/07/14
+ */
+public interface Product {
+}
---
+++ src/design/pattern/simplefactory/SimpleFactory.java
@@ -0,0 +1,18 @@
+package design.pattern.simplefactory;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/07/14
+ */
+public class SimpleFactory {
+    public Product createProduct(int type) {
+        if (1 == type) {
+            return new ConcreteProduct1();
+        } else if (2 == type) {
+            return new ConcreteProduct2();
+        } else {
+            return new ConcreteProduct();
+        }
+    }
+}
