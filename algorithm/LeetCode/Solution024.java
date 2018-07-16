b633d67999517102482299ba88f35ee78bf66314
mayuan <mayuanucas@qq.com>
2018-07-16 17:17:10 +0800

    两两交换链表中的节点

---
+++ algorithm/LeetCode/Solution024.java
@@ -0,0 +1,34 @@
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/07/15
+ */
+public class Solution024 {
+    public ListNode swapPairs(ListNode head) {
+        if (null == head || null == head.next) {
+            return head;
+        }
+
+        ListNode temp = new ListNode(-1);
+        temp.next = head;
+        ListNode p = temp;
+
+        while (null != p.next && null != p.next.next) {
+            ListNode t = p.next;
+            p.next = t.next;
+            t.next = p.next.next;
+            p.next.next = t;
+            p = t;
+        }
+        return temp.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
