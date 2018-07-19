0cb76dd03bccb41363646443bf78c162f2159dec
mayuan <mayuanucas@qq.com>
2018-07-19 20:08:33 +0800

    Hash 学习

--- src/my/collection/MyHashMap.java
+++ src/my/collection/MyHashMap.java
@@ -1,6 +1,7 @@
 package my.collection;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -11,13 +12,13 @@ import java.util.Map;
 public class MyHashMap {
 
     public static void main(String[] args) {
-        Map<String, String> map1 = new HashMap<>(32);
+        Map<String, String> map1 = new HashMap<>(4);
 
-        map1.put("a", "优秀");
-        map1.put("b", "良好");
-        map1.put("c", "及格");
-        map1.put("d", "差");
-        map1.put("e", "极差");
+        map1.put("aa", "优秀");
+        map1.put("ab", "良好");
+        map1.put("cb", "及格");
+        map1.put("db", "差");
+        map1.put("ea", "极差");
 
         // map 的遍历
         // 1. jdk1.8
@@ -29,6 +30,23 @@ public class MyHashMap {
         for (Map.Entry entry : map1.entrySet()) {
             System.out.println("key=" + entry.getKey() + ", value=" + entry.getValue());
         }
+        System.out.println("********************************");
+
+
+        // 有序 hash
+        Map<String, String> map2 = new LinkedHashMap<>();
+        map2.put("za", "优秀");
+        map2.put("bz", "良好");
+        map2.put("cz", "及格");
+        map2.put("zd", "差");
+        map2.put("ez", "极差");
+
+        for (Map.Entry entry : map2.entrySet()) {
+            System.out.println("key=" + entry.getKey() + ", value=" + entry.getValue());
+        }
+        System.out.println("********************************");
+
+
 
     }
 
