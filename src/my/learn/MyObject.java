c757e98edab7912e4e6889a04e20c57793c6df47
mayuan <mayuanucas@qq.com>
2018-07-21 22:59:28 +0800

    Object 类学习

---
+++ src/my/learn/MyObject.java
@@ -0,0 +1,20 @@
+package my.learn;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/07/20
+ */
+public class MyObject {
+
+    public MyObject() {
+    }
+
+    public static void main(String[] args) {
+        Object object = new Object();
+        System.out.println(Object.class);
+        System.out.println("abc".getClass());
+
+        System.out.println(object.toString());
+    }
+}
