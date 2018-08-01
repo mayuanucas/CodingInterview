e754fb0d10b7dcbcc427a64003d6011699afad5b
mayuan <mayuanucas@qq.com>
2018-08-01 17:00:39 +0800

    ClassLoader学习

---
+++ src/my/learn/ClassLoader.java
@@ -0,0 +1,31 @@
+package my.learn;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/08/01
+ */
+public class ClassLoader {
+    public static void main(String[] args) {
+        ClassLoader.bootstrapClassLoader();
+        System.out.println("********************");
+        ClassLoader.extClassLoader();
+        System.out.println("********************");
+        ClassLoader.applicationClassLoader();
+        System.out.println("********************");
+    }
+
+    public static void bootstrapClassLoader() {
+        System.out.println("BootstrapClassLoader:" + String.class.getClassLoader());
+        System.out.println(System.getProperty("sun.boot.class.path"));
+    }
+
+    public static void extClassLoader() {
+        System.out.println(System.getProperty("java.ext.dirs"));
+    }
+
+    public static void applicationClassLoader() {
+        System.out.println(System.getProperty("java.class.path"));
+        System.out.println(ClassLoader.class.getClassLoader());
+    }
+}
