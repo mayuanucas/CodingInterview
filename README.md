db3e15ad930c97ff45406a8b417c4ee0af7d7022
mayuan <mayuanucas@qq.com>
2018-08-03 19:28:23 +0800

    第k个排列

--- README.md
+++ README.md
@@ -12,12 +12,13 @@ LeetCode 解题记录: [LeetCode解题](src/main/java/algorithm/leetcode)
 
 | 题号  | 题目   | 难度  |
 |:---:|:---- |:---:|
-| 17   | [电话号码的字母组合](src/main/java/algorithm/leetcode/Solution017.java) |  中等  |
-| 22   | [括号生成](src/main/java/algorithm/leetcode/Solution022.java) |  中等  |
-| 39   | [组合总和](src/main/java/algorithm/leetcode/Solution039.java) |  中等  |
-| 40   | [组合总和2](src/main/java/algorithm/leetcode/Solution040.java) |  中等  |
-| 46   | [全排列](src/main/java/algorithm/leetcode/Solution046.java) |  中等  |
-| 47   | [全排列2](src/main/java/algorithm/leetcode/Solution047.java) |  中等  |
+| 017   | [电话号码的字母组合](src/main/java/algorithm/leetcode/Solution017.java) |  中等  |
+| 022   | [括号生成](src/main/java/algorithm/leetcode/Solution022.java) |  中等  |
+| 039   | [组合总和](src/main/java/algorithm/leetcode/Solution039.java) |  中等  |
+| 040   | [组合总和2](src/main/java/algorithm/leetcode/Solution040.java) |  中等  |
+| 046   | [全排列](src/main/java/algorithm/leetcode/Solution046.java) |  中等  |
+| 047   | [全排列2](src/main/java/algorithm/leetcode/Solution047.java) |  中等  |
+| 060   | [第k个排列](src/main/java/algorithm/leetcode/Solution060.java) |  中等  |
 
 
 ### [51nod](http://www.51nod.com/)
---
+++ src/main/java/algorithm/leetcode/Solution060.java
@@ -0,0 +1,63 @@
+package algorithm.leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/08/03
+ */
+public class Solution060 {
+
+    public static void main(String[] args) {
+        Solution060 test = new Solution060();
+        System.out.println(test.getPermutation(4, 9));
+    }
+
+    /**
+     * 在n!个排列中，第一位的元素总是(n-1)!一组出现的，也就说如果a = (k-1) / (n-1)!，那么排列的最开始一个元素一定是numbers[a]
+     * k1 = k
+     * a1 = (k1-1) / (n-1)!     number[a1]
+     * k2 = k1 - a1 * (n-1)!
+     * a2 = (k2-1) / (n-2)!     number[a1]
+     *
+     * @param n
+     * @param k
+     * @return
+     */
+    public String getPermutation(int n, int k) {
+        // 存储阶乘的数组
+        int[] factorial = new int[n + 1];
+        factorial[0] = 1;
+        int sum = 1;
+        for (int i = 1; i <= n; i++) {
+            sum *= i;
+            factorial[i] = sum;
+        }
+
+        // k 越界
+        if (k > factorial[n]) {
+            return "";
+        }
+
+        List<Integer> numbers = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            numbers.add(i);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        // 可以认为是:依次确定从左边第1位到右边第n位数字的过程
+        for (int i = 1; i <= n; i++) {
+            int a = (k - 1) / factorial[n - i];
+            stringBuilder.append(numbers.get(a));
+            // 添加该数字后,需要从列表中把该数字删除
+            numbers.remove(a);
+            // 此处不用取余,避免K为-1
+            k = k - a * factorial[n - i];
+        }
+        return stringBuilder.toString();
+    }
+
+
+}
