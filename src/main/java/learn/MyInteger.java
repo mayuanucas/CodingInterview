ec20a46d77911c92a4319e0cf28f79234d5da178
mayuan <mayuanucas@qq.com>
2018-08-04 23:26:55 +0800

    Integer包装类

---
+++ src/main/java/learn/MyInteger.java
@@ -0,0 +1,30 @@
+package learn;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/08/04
+ */
+public class MyInteger {
+
+    public static void main(String[] args) {
+        Integer a = 1;
+        Integer b = 2;
+        Integer c = 3;
+        Integer d = 3;
+        Integer e = 321;
+        Integer f = 321;
+        Long g = 3L;
+        Long h = 2L;
+
+        System.out.println(c == d);
+        System.out.println(e == f);
+        System.out.println(c == (a + b));
+        System.out.println(c.equals(a + b));
+        System.out.println(g == (a + b));
+        System.out.println(g.equals(a + b));
+        System.out.println(g.equals(a + h));
+
+    }
+
+}
