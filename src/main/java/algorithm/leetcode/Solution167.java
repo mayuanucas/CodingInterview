68035a3f5d09223176dd43c59882e8029be30630
mayuan <mayuanucas@qq.com>
2018-08-17 18:52:57 +0800

    两数之和 II - 输入有序数组

---
+++ src/main/java/algorithm/leetcode/Solution167.java
@@ -0,0 +1,41 @@
+package algorithm.leetcode;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/08/17
+ */
+public class Solution167 {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        Solution167 test = new Solution167();
+        for (int t : test.twoSum(nums, target)) {
+            System.out.println(t);
+        }
+    }
+
+    public int[] twoSum(int[] numbers, int target) {
+        int[] answer = new int[2];
+        if (null == numbers || 0 >= numbers.length) {
+            return answer;
+        }
+
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int temp = numbers[left] + numbers[right];
+            if (target == temp) {
+                answer[0] = left + 1;
+                answer[1] = right + 1;
+                break;
+            } else if (target > temp) {
+                ++left;
+            } else {
+                --right;
+            }
+        }
+        return answer;
+    }
+}
