d4b93ff3b5bd0c41d54e0de4d808220b4ae50c9b
mayuan <mayuanucas@qq.com>
2018-09-06 22:11:18 +0800

    赛码主类

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+/**
+ * @author: mayuan
+ * @desc:
+ * @date: 2018/09/06
+ */
+public class Main {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+    }
+}
