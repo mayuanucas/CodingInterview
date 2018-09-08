b252010fb3779bd12fa93c65221b9edaa7360035
mayuan <mayuanucas@qq.com>
2018-09-08 23:55:04 +0800

    多测试用例

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 /**
  * @author: mayuan
@@ -8,7 +8,13 @@ import java.util.*;
 public class Main {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
+        int testNumber = Integer.parseInt(scanner.nextLine());
 
+        while (0 < testNumber--) {
+            String[] temp = scanner.nextLine().split("\\s+");
+            int n = Integer.parseInt(temp[0]);
+            int m = Integer.parseInt(temp[1]);
+
+        }
     }
 }
