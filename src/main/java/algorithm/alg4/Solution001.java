b958f482ad14156d0235837259dbff9d9a29380b
mayuan <mayuanucas@qq.com>
2018-10-22 19:05:34 +0800

    最大公约数和最小公倍数

--- src/main/java/algorithm/alg4/Solution001.java
+++ src/main/java/algorithm/alg4/Solution001.java
@@ -8,38 +8,50 @@ package algorithm.alg4;
 public class Solution001 {
 
     public static void main(String[] args) {
-        System.out.println(gcd(12, 8));
-        System.out.println(lcm(12, 8));
+        System.out.println(gcd1(12, 18));
+        System.out.println(gcd1(18, 12));
+        System.out.println(gcd2(12, 18));
+        System.out.println(gcd2(18, 12));
+        System.out.println(lcm(12, 18));
     }
 
     /**
-     * 求两个数的最大公约数
+     * 求 a 与 b 这两个数字的最大公约数(递归)
+     * 欧几里得算法:
+     * 计算两个非负整数 a 和 b 的最大公约数,若 b 为 0,则最大公约数为 a.
      *
-     * @param maxNumber 较大的数字
-     * @param minNumber 较小的数字
+     * @param a
+     * @param b
      * @return
      */
-    public static int gcd(int maxNumber, int minNumber) {
-        if (0 == minNumber) {
-            return maxNumber;
-        }
-
-        while (0 != minNumber) {
-            int tmp = maxNumber % minNumber;
-            maxNumber = minNumber;
-            minNumber = tmp;
-        }
-        return maxNumber;
+    public static int gcd1(int a, int b) {
+        return 0 == b ? a : gcd1(b, a % b);
     }
 
     /**
-     * 求两个数字的最小公倍数
+     * 求 a 与 b 这两个数字的最大公约数(非递归)
      *
-     * @param maxNumber 较大的数字
-     * @param minNumber 较小的数字
+     * @param a
+     * @param b
      * @return
      */
-    public static int lcm(int maxNumber, int minNumber) {
-        return maxNumber * minNumber / gcd(maxNumber, minNumber);
+    public static int gcd2(int a, int b) {
+        while (0 != b) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
+    /**
+     * 求 a 与 b 这两个数字的最小公倍数
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int lcm(int a, int b) {
+        return a * b / gcd1(a, b);
     }
 }
