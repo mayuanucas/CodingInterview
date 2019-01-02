1572f2fb9fb338c076d635742a2c718c57989479
mayuan <mayuanucas@qq.com>
2019-01-02 21:19:16 +0800

    删除链表的倒数第N个节点

--- README.md
+++ README.md
@@ -17,6 +17,7 @@
 | 015  | 数组、双指针 | [三数之和](src/main/java/algorithm/leetcode/Solution015.java) | 中等 |
 | 016  | 数组、双指针 | [最接近的三数之和](src/main/java/algorithm/leetcode/Solution016.java) | 中等 |
 | 018  | 数组、哈希表、双指针 | [四数之和](src/main/java/algorithm/leetcode/Solution018.java) | 中等 |
+| 019  | 链表、双指针 | [删除链表的倒数第N个节点](src/main/java/algorithm/leetcode/Solution019.java) | 中等 |
 | 026  | 数组、双指针 | [删除排序数组中的重复项](src/main/java/algorithm/leetcode/Solution026.java) | 简单 |
 | 206  |  | [反转链表](src/main/java/algorithm/leetcode/Solution206.java) | 简单 |
 | 235  |              | [二叉搜索树的最近公共祖先](src/main/java/algorithm/leetcode/Solution235.java) | 简单 |
--- src/main/java/algorithm/leetcode/Solution019.java
+++ src/main/java/algorithm/leetcode/Solution019.java
@@ -2,16 +2,17 @@ package algorithm.leetcode;
 
 /**
  * @author: mayuan
- * @desc:
+ * @desc: 删除链表的倒数第N个节点
  * @date: 2018/07/15
  */
 public class Solution019 {
     /**
      * 1.我们要删除单链表中的一个元素（在不变动其值得情况下），我们只能是先找到它的前一个节点，然后重新连接。在这里为了防止删除的是正数第一个节点，所以我们有必要加一个临时的头结点temp。
-     *
-     * 2.采用快慢指针的思路，去定位倒数第n个节点。两个游标指针fast,slow。先让fast走n步，然后二者同时往下走。最终slow走到要删除节点的前一个元素，而fast走向NULL。
-     *
+     * <p>
+     * 2.采用快慢指针的思路，去定位倒数第n个节点。两个游标指针fast,slow。先让fast走n步，然后二者同时往下走。最终fast走到NULL,而slow指针走到要删除节点的前一个元素。
+     * <p>
      * 3.执行删除操作。返回temp->next.
+     *
      * @param head
      * @param n
      * @return
@@ -39,5 +40,14 @@ public class Solution019 {
         slow.next = fast.next;
         fast.next = null;
         return temp.next;
+    }
+
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
     }
 }
