5042ffe42132990690990feb48e769367a029e45
mayuan <mayuanucas@qq.com>
2019-01-02 09:16:30 +0800

    两数之和

--- src/main/java/algorithm/leetcode/Solution001.java
+++ src/main/java/algorithm/leetcode/Solution001.java
@@ -12,20 +12,23 @@ import java.util.Map;
  */
 public class Solution001 {
 
+    /**
+     * 依次顺序扫描数组,每次从哈希表中查询是否存在对应的相加之和为 target 的数字,
+     * 存在则直接将它们的下标放入数组返回,不存在则将当前数字和下标放入哈希表.
+     * key -> 该数字
+     * value -> 该数字的下标
+     */
     public int[] twoSum(int[] nums, int target) {
         if (null == nums || 0 >= nums.length) {
             return null;
         }
 
-        /*
-        依次顺序扫描数组,每次从哈希表中查询是否存在对应的相加之和为 target 的数字,
-        存在则直接将它们的下标放入数组返回,不存在则将当前数字和下标放入哈希表.
-         */
         Map<Integer, Integer> map = new HashMap<>(nums.length);
+        int pos;
         for (int i = 0; i < nums.length; ++i) {
-            int cur = target - nums[i];
-            if (map.containsKey(cur)) {
-                return new int[]{i, map.get(cur)};
+            int number = target - nums[i];
+            if (-1 != (pos = map.getOrDefault(number, -1))) {
+                return new int[]{i, pos};
             } else {
                 map.put(nums[i], i);
             }
