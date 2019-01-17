ebb0e4e0033f942ac3c2e5f55aaf0bd7f57e0446
mayuan <mayuanucas@qq.com>
2019-01-18 00:18:57 +0800

    摆动排序 II

--- README.md
+++ README.md
@@ -44,6 +44,7 @@
 | 274  | 排序、哈希表 | [H指数](src/main/java/algorithm/leetcode/Solution274.java) | 中等 |
 | 283  | 数组、双指针 | [移动零](src/main/java/algorithm/leetcode/Solution283.java) | 简单 |
 | 287  | 数组、双指针、二分查找 | [寻找重复数](src/main/java/algorithm/leetcode/Solution287.java) | 中等 |
+| 324  | 排序 | [摆动排序 II](src/main/java/algorithm/leetcode/Solution324.java) | 中等 |
 | 344  | 字符串、双指针 | [反转字符串](src/main/java/algorithm/leetcode/Solution344.java) | 简单 |
 | 345  | 字符串、双指针 | [反转字符串中的元音字母](src/main/java/algorithm/leetcode/Solution345.java) | 简单 |
 | 349  | 排序、哈希表、双指针、二分查找 | [两个数组的交集](src/main/java/algorithm/leetcode/Solution349.java) | 简单 |
---
+++ src/main/java/algorithm/leetcode/Solution324.java
@@ -0,0 +1,105 @@
+package algorithm.leetcode;
+
+/**
+ * @author: mayuan
+ * @desc: 摆动排序 II
+ * @date: 2019/01/14
+ */
+public class Solution324 {
+
+    public static void main(String[] args) {
+        int[] array = {1, 5, 1, 1, 6, 4};
+
+        new Solution324().wiggleSort(array);
+
+        for (int e : array) {
+            System.out.print(e + " ");
+        }
+    }
+
+    public void wiggleSort(int[] nums) {
+        if (null == nums || 1 >= nums.length) {
+            return;
+        }
+
+        // 找到数组的中位数,该数字可以把全部数据分为两组:
+        // A组数值都小于该中位数,B组数值都大于等于该中位数
+        // findKthLargest调用之后,左侧数字均小于轴,右侧数字均大于轴
+        int media = findKthLargest(nums, (nums.length + 1) / 2);
+        // 借鉴荷兰国旗问题(3类数字排序)
+        int i = 0, j = 0, k = nums.length - 1;
+        while (j <= k) {
+            // 奇数索引位置的值需要大于中位数,如果发现该位置数字值大于中位数,赶紧把该数字移动到前面
+            if (nums[A(j, nums.length)] > media) {
+                swap(nums, A(i, nums.length), A(j, nums.length));
+                ++i;
+                ++j;
+            } else if (nums[A(j, nums.length)] < media) {
+                swap(nums, A(j, nums.length), A(k, nums.length));
+                --k;
+            } else {
+                ++j;
+            }
+        }
+    }
+
+    /**
+     * 索引映射变换,借鉴轴划分的思想.
+     * 轴之前位置的数字值均大于轴,轴之后位置的数字均小于轴
+     *
+     * @param i
+     * @param n
+     * @return
+     */
+    public int A(int i, int n) {
+        int index = (1 + 2 * i) % (n | 1);
+        return index;
+    }
+
+    /**
+     * 找到数组中第K个大的数字
+     *
+     * @param array
+     * @param k
+     * @return
+     */
+    public int findKthLargest(int[] array, int k) {
+        // 数组是从小到大排列,第K个大的数字,其下标索引即为
+        // 从左往右索引为 array.length - k 的数字
+        k = array.length - k;
+        int l = 0, r = array.length - 1;
+        while (l < r) {
+            int index = partition(array, l, r);
+            if (k == index) {
+                break;
+            } else if (k > index) {
+                l = index + 1;
+            } else {
+                r = index - 1;
+            }
+        }
+        return array[k];
+    }
+
+    public int partition(int[] array, int left, int right) {
+        int pivot = array[left];
+        while (left < right) {
+            while (left < right && array[right] >= pivot) {
+                --right;
+            }
+            array[left] = array[right];
+            while (left < right && array[left] <= pivot) {
+                ++left;
+            }
+            array[right] = array[left];
+        }
+        array[left] = pivot;
+        return left;
+    }
+
+    public void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+}
