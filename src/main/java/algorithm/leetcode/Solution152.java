ca5cbf3b443c60a518b99442552b2fe3cd0de86e
mayuan <mayuanucas@qq.com>
2019-02-20 22:01:06 +0800

    乘积最大子序列

--- src/main/java/algorithm/leetcode/Solution152.java
+++ src/main/java/algorithm/leetcode/Solution152.java
@@ -13,6 +13,10 @@ public class Solution152 {
 
         int ans = nums[0];
         for (int i = 1, mx = nums[0], mn = nums[0]; i < nums.length; ++i) {
+            // 以当前位置结尾,乘积最大的连续子序列有以下几种情况:
+            // 1. 当前元素为正数,从前一状态挑选最大值转移过来
+            // 2. 当前元素为负数,从前一状态挑选最小值转移过来(负负得正)
+            // 3. 前一状态为0, 从前一状态或当前元素选择最大值
             int a = mx * nums[i];
             int b = mn * nums[i];
             mx = Math.max(Math.max(a, b), nums[i]);
