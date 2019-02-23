de789f52c13c6d089e9d22513b11e988fbf0ce5b
mayuan <mayuanucas@qq.com>
2019-02-23 21:44:13 +0800

    区域和检索 - 数组不可变

--- src/main/java/algorithm/leetcode/Solution303.java
+++ src/main/java/algorithm/leetcode/Solution303.java
@@ -2,24 +2,14 @@ package algorithm.leetcode;
 
 /**
  * @author: mayuan
- * @desc:
+ * @desc: 区域和检索 - 数组不可变
  * @date: 2018/08/22
  */
 public class Solution303 {
-    public static void main(String[] args) {
-        int[] nums = {-2, 0, 3, -5, 2, -1};
 
-        NumArray numArray = new NumArray(nums);
-        System.out.println(numArray.sumRange(2, 5));
-        System.out.println(numArray.sumRange(0, 5));
-        System.out.println(numArray.sumRange(0, 2));
-    }
-}
-
-class NumArray {
     private int[] nums;
 
-    public NumArray(int[] nums) {
+    public Solution303(int[] nums) {
         this.nums = nums;
         for (int i = 1; i < nums.length; ++i) {
             this.nums[i] = this.nums[i - 1] + this.nums[i];
