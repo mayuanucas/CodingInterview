bb19f3a62c9fc997479802898d06e7bc67196a76
mayuan <mayuanucas@qq.com>
2019-02-28 21:44:41 +0800

    等差数列划分

--- src/main/java/algorithm/leetcode/Solution413.java
+++ src/main/java/algorithm/leetcode/Solution413.java
@@ -2,7 +2,7 @@ package algorithm.leetcode;
 
 /**
  * @author: mayuan
- * @desc:
+ * @desc: 等差数列划分
  * @date: 2018/08/22
  */
 public class Solution413 {
@@ -14,19 +14,45 @@ public class Solution413 {
     }
 
     public int numberOfArithmeticSlices(int[] A) {
+        if (null == A || 3 > A.length) {
+            return 0;
+        }
+
+        // dp[i]表示以i位置结尾构成的等差子序列的个数
+        int[] dp = new int[A.length];
+        dp[0] = 0;
+        dp[1] = 0;
+        int sum = 0;
+
+        for (int i = 2; i < A.length; ++i) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                // 增加1表示:增加了(dp[i-2],dp[i-1],dp[i]) 这个序列
+                dp[i] = dp[i - 1] + 1;
+            } else {
+                dp[i] = 0;
+            }
+
+            sum += dp[i];
+        }
+        return sum;
+    }
+
+    public int numberOfArithmeticSlices2(int[] A) {
         if (null == A || 0 >= A.length) {
             return 0;
         }
 
-        int added = 0;
         int sum = 0;
+        // 上一阶段值
+        int psv = 0;
         for (int i = 2; i < A.length; ++i) {
             if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
-                added += 1;
-                sum += added;
+                psv += 1;
             } else {
-                added = 0;
+                psv = 0;
             }
+
+            sum += psv;
         }
 
         return sum;
