e20ecc3fe91c93c89f93d226139fc4d9c1d80ebe
mayuan <mayuanucas@qq.com>
2019-03-22 17:01:39 +0800

    LRU算法的实现

--- src/main/java/algorithm/note/LRU.java
+++ src/main/java/algorithm/note/LRU.java
@@ -8,18 +8,18 @@ import java.util.Map;
  * @desc: LRU算法的实现
  * @date: 2018/09/14
  */
-public class LRU<K, V> {
+public class LRU<K> {
 
     private static final float loadFactor = 0.75F;
     private final int capacity;
 
-    private LinkedHashMap<K, V> map;
+    private LinkedHashMap<K, K> map;
 
     public LRU(int size) {
         this.capacity = size;
         int cap = (int) Math.ceil(size / loadFactor) + 1;
 
-        map = new LinkedHashMap<K, V>(cap, loadFactor, true) {
+        map = new LinkedHashMap<K, K>(cap, loadFactor, true) {
             @Override
             protected boolean removeEldestEntry(Map.Entry entry) {
                 return size() > capacity;
@@ -27,11 +27,11 @@ public class LRU<K, V> {
         };
     }
 
-    public V get(K key) {
+    public K get(K key) {
         return map.get(key);
     }
 
-    public void put(K key, V value) {
-        map.put(key, value);
+    public void put(K key) {
+        map.put(key, key);
     }
 }
